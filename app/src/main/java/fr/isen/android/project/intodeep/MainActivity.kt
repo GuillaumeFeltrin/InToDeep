ac3aa81717b3e2d7d9592280c249c555f8741f73
Paul-Adrien Charnoz <paul-adrien.charnoz@isen.yncrea.fr>
2020-02-11 15:12:38 +0100

    add specific button to view loading activity content

--- app/src/main/java/fr/isen/android/project/intodeep/MainActivity.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        switchActivityButton.setOnClickListener {
+        switchActivityLoadingButton.setOnClickListener {
             startActivity(Intent(this, loading::class.java))
         }
     }
