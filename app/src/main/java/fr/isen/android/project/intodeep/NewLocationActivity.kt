911440b42ac2f17841f09d2138acdbc631a7cd59
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2020-02-17 14:48:49 +0100

    Adding 4 sites in database to test markers

--- app/src/main/java/fr/isen/android/project/intodeep/NewLocationActivity.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/NewLocationActivity.kt
@@ -15,59 +15,21 @@ class NewLocationActivity : AppCompatActivity() {
     lateinit var storage: FirebaseStorage
 
     lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_location)
         database = FirebaseDatabase.getInstance().reference
         storage = FirebaseStorage.getInstance()
-        buttonTest.setOnClickListener {
-            downloadLocations()
-        }
+        writeNewSite("1", "Le Togo", "43.168611", "6.60222")
+        writeNewSite("2", "Le Donator", "42.998889", "6.277778")
+        writeNewSite("3", "Le Grec", "42.993611", "6.278333")
+        writeNewSite("4", "Le Spahis", "43.111667", "6.407778")
+
     }
 
-    fun downloadLocations(){
-        val locations = database.child("locations")
-
-        locations.addValueEventListener(object : ValueEventListener
-        {
-            override fun onCancelled(p0: DatabaseError) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                val locations = ArrayList<LocationClass>()
-
-                for(postSnapshot in p0.children )
-                {
-                    val p = postSnapshot.value as HashMap<String, String>
-                    val locationId = p["locationId"]
-                    val name = p["name"]
-                    val latitude = p["latitude"]
-                    val longitude = p["longitude"]
-                    val id = p["id"]
-                    id?.let{
-                        val publi = LocationClass(id, locationId, name, latitude, longitude)
-                        locations.add(publi)
-                    }
-                }
-                locations.reverse()
-                recyclerAndDatabaseHandler(locations)
-            }
-        })
-    }
-
-    fun recyclerAndDatabaseHandler(locations : ArrayList<LocationClass>){
-        val tabLocations = LastLocations()
-        val publi = LocationClass(
-            id = "123", locationId = "lol", name = "Le Donator",
-            latitude = "42.998889", longitude = "6.277778"
-         )
-        val publi2 = LocationClass(
-            id = "1234", locationId = "lol", name = "Le Togo",
-            latitude = "43.168611", longitude = "6.60222"
-         )
-        //tabLocations.results = locations
-        locations.add(publi)
-        locations.add(publi2)
+    private fun writeNewSite(id: String, name: String, latittude: String?, longitude: String) {
+        val location = LocationClass(id, name, latittude, longitude)
+        database.child("diving_site").child(id).setValue(location)
     }
 }
--- app/src/main/java/fr/isen/android/project/intodeep/classes/LocationClass.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/classes/LocationClass.kt
@@ -5,7 +5,6 @@ import com.google.firebase.database.IgnoreExtraProperties
 @IgnoreExtraProperties
 class LocationClass (
     var id: String,
-    var locationId: String?,
     var name: String?,
     var latitude: String?,
     var longitude: String?
