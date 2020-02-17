79681ff02188b105851666ae00f05c02cb1354d3
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2020-02-17 08:40:38 +0100

    Google authentication works

--- app/src/main/java/fr/isen/android/project/intodeep/LoginActivity.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/LoginActivity.kt
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val RC_SIGN_IN = 1
+    val TAG = "CreateAccount"
+    var googleApiClient: GoogleApiClient? = null
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,19 +30,30 @@ class LoginActivity : AppCompatActivity() {
         logInButton.setOnClickListener {
             doLogin(textInputEmail.text.toString(), textInputPassword.text.toString())
         }
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
+        loginGoogleButton.setOnClickListener {
+            googleLogin()
+        }
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.request_client_id))
             .requestEmail()
             .build()
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this  /* OnConnectionFailedListener */) { }
+            .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+            .build()
+
+
+        /*mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
         val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        startActivityForResult(signInIntent, RC_SIGN_IN)*/
     }
+
 
     public override fun onStart() {
         super.onStart()
         val currentUser = auth.currentUser
-        if (currentUser != null){
+        if (currentUser != null) {
             intent = Intent(this, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
@@ -44,7 +61,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun doLogin(email: String, password: String){
+    fun doLogin(email: String, password: String) {
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -56,52 +73,50 @@ class LoginActivity : AppCompatActivity() {
                     startActivity(intent)
                 } else {
                     Log.w("testemail", "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
 
-    fun googleSignIn(){
-
-
+    private fun googleLogin() {
+        Log.i(TAG, "Starting Google LogIn Flow.")
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        Log.i(TAG, "Got Result code ${requestCode}.")
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            Log.i(TAG, "With Google LogIn, is result a success? ${result.isSuccess}.")
+            if (result.isSuccess) {
                 // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)
-                firebaseAuthWithGoogle(account!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w("googletest", "Google sign in failed", e)
-                // ...
+                firebaseAuthWithGoogle(result.signInAccount!!)
+            } else {
+                Toast.makeText(this, "Some error occurred.", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
 
     private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        Log.d("testlol", "firebaseAuthWithGoogle:" + acct.id!!)
-
+        Log.i(TAG, "Authenticating user with firebase.")
         val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Log.d("succesgoogle", "signInWithCredential:success")
-                    val user = auth.currentUser
-
-                } else {
-                    Log.w("erreurgoogle", "signInWithCredential:failure", task.exception)
-                    Snackbar.make(activity_login, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
-
-                }
-
-                // ...
+        auth?.signInWithCredential(credential)?.addOnCompleteListener(this) { task ->
+            Log.i(TAG, "Firebase Authentication, is result a success? ${task.isSuccessful}.")
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.e(TAG, "Authenticating with Google credentials in firebase FAILED !!")
             }
-
+        }
     }
+}
