9be6984dd1f764963ad9b4f6daee6c0db574e41d
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2020-03-07 10:52:16 +0100

    Nothing change

--- app/src/main/java/fr/isen/android/project/intodeep/HomeActivity.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/HomeActivity.kt
@@ -3,12 +3,16 @@ package fr.isen.android.project.intodeep
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    var googleApiClient: GoogleApiClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
