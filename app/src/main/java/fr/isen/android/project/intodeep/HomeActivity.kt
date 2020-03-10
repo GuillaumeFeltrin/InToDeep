7df59ec5a378c1dc4572831c9a342aaaf0938e8f
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2020-03-10 11:48:08 +0100

    Adding navbar on Home page

--- app/src/main/java/fr/isen/android/project/intodeep/HomeActivity.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/HomeActivity.kt
@@ -3,16 +3,20 @@ package fr.isen.android.project.intodeep
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    lateinit var toolbar: ActionBar
     var googleApiClient: GoogleApiClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -25,6 +29,30 @@ class HomeActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.add_item -> {
+                intent= Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.perso_item -> {
+                intent= Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.feed_item -> {
+                intent= Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                true
+            }
+        }
+        false
+    }
 }
