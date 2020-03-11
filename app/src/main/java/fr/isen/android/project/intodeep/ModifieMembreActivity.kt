2a9692ff165e997cfd6b88590f0964a2f029a22c
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2020-03-11 20:40:35 +0100

    Modification

--- app/src/main/java/fr/isen/android/project/intodeep/ModifieMembreActivity.kt
+++ app/src/main/java/fr/isen/android/project/intodeep/ModifieMembreActivity.kt
@@ -1,12 +1,17 @@
 package fr.isen.android.project.intodeep
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.core.text.set
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_member.*
 import kotlinx.android.synthetic.main.activity_modifie_membre.*
+import kotlinx.android.synthetic.main.activity_modifie_membre.myBackgroundLayout
 
 class ModifieMembreActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
@@ -15,9 +20,22 @@ class ModifieMembreActivity : AppCompatActivity() {
     private val table_name:String ="profile"
     var id:String=""
 
+    lateinit var toolbar: ActionBar
+    lateinit var frameAnimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modifie_membre)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        frameAnimation = myBackgroundLayout.getBackground() as AnimationDrawable
+        frameAnimation.setEnterFadeDuration(4500)
+        frameAnimation.setExitFadeDuration(4500)
+        frameAnimation.start()
+
         auth = FirebaseAuth.getInstance()
 
         id = id_maker(auth.currentUser?.email.toString())
@@ -106,5 +124,31 @@ class ModifieMembreActivity : AppCompatActivity() {
         gauche = gauche.replace(".","")
 
         return gauche
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.memo_item -> {
+                intent= Intent(this, MemoActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.perso_item -> {
+                intent= Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.feed_item -> {
+                intent= Intent(this, GoogleMapInfoWindowActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.add_item -> {
+                intent= Intent(this, AddSpotActivity::class.java)
+                startActivity(intent)
+                true
+            }
+        }
+        false
     }
 }
